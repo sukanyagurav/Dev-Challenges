b9d950cd701bbd1490a5d50dcd2eeb7498bb3c24
sukanya gurav <sukanyagurav6@gmail.com>
2024-11-02 21:50:12 +0530

    updated meet landing page

--- meet_the_team_section/README.md
+++ meet_the_team_section/README.md
@@ -1,8 +1,48 @@
-# React + Vite
+# Meet the team section
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Welcome! 👋
 
-Currently, two official plugins are available:
+- [GitHub Url](https://github.com/sukanyagurav/Dev-Challenges/tree/main/meet_the_team_section)
+- [Netlify](https://galleria5867.netlify.app/)
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Overview
+I built a "Meet the Team" section for a website as part of a design challenge. The layout features five images, each representing a team member along with their name and job title. My goal with this project is to practice using grid layout and positioning techniques, specifically position: absolute and position: relative, to create a responsive and visually appealing design.
+
+### Meet the team section Desktop View     
+<img src="./public/desktop preview.png" width="100%" /> 
+
+<div align="center">
+    <h2> Tech stack used</h2>
+</div>
+
+<div align="center">
+
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)   ![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white)
+</div>
+
+## User stories
+✅ Create a meet the team section website with 5 images representing 5 members, each including their name and job title.
+
+✅ Utilize grid layout in CSS to structure the layout.
+
+✅ Style the layout to be responsive and adapt to different screen sizes.
+
+✅ Apply clean spacing and typography to enhance the overall design.
+
+✅ Ensure the website is accessible and user-friendly.
+
+✅ Test the website on different devices and screen sizes to ensure responsiveness.
+
+✅ Optimize the website for performance and loading speed.
+
+✅ Use semantic HTML elements to structure the content and improve SEO.
+
+✅ Apply CSS styles to enhance the visual presentation of the team member elements.
+
+✅ (Optional) Add hover effects or animations to make the website more interactive.
+
+✅ Validate the HTML and CSS code to ensure it follows best practices and standards.
+
+✅ Document the code and provide clear instructions on how to run and use the website.
+
+✅ Deploy the website to make it accessible for everyone.
--- meet_the_team_section/src/App.jsx
+++ meet_the_team_section/src/App.jsx
@@ -1,45 +1,122 @@
-import arrowUp from '/arrow up right.svg'
-function App() {
+import arrowUp from "/arrow up right.svg";
 
+function App() {
   return (
     <>
-      <main className="grid">
-        <div>
-          <h2>Our team</h2>
-          <h1>Meet the brain</h1>
-          <p>We are proud to have them as part of our community and look forward to continuing to push the boundaries of what's possible in the world of digital art.</p>
+      <main className="bg-design bg-[#F3F4F6]    relative grid rounded-lg overflow-hidden place-items-center my-9 md:grid-cols-2 lg:grid-cols-4 max-w-[1200px] w-full mx-auto shadow-lg ">
+ 
+        <div className=" p-8 pb-28 md:col-span-2  relative z-10">
+          <h2 className="text-blue-400 text-md">Our team</h2>
+          <h1 className="text-4xl my-6">Meet the brain</h1>
+          <p className="text-[#394150] font-semibold">
+            We are proud to have them as part of our community and look forward
+            to continuing to push the boundaries of what's possible in the world
+            of digital art.
+          </p>
         </div>
-        <div>
-          <img src="" alt="" />
-          <h3>Liam Novak</h3>
-          <h4>Software Engineer</h4>
+        <div className="w-full relative min-h-[300px]  bg-[#C1B6F8] lg:min-h-full flex items-end card overflow-hidden">
+          <picture className="relative inline-block z-30">
+            <source
+              srcset="/person_1.png"
+              aria-hidden="true"
+              alt=""
+              media="(max-width:850px)"
+            />
+            <img
+              src="/person_1@2x.png"
+              alt=""
+              className="brightness-90 md:max-w-[300px] relative z-20 transition-all duration-300 ease-in-out"
+            />
+          </picture>
+          <div className="absolute bottom-5 left-5 text-[#FFFEFE] z-50">
+            <h3 className="font-bold text-xl">Liam Novak</h3>
+            <h4 className="text-md mt-2">Software Engineer</h4>
+          </div>
         </div>
-        <div>
-          <img src="" alt="" />
-          <h3>Sophia Moretti</h3>
-          <h4>Business Development Manager</h4>
+        <div className="w-full relative min-h-[300px]  bg-[#EDACD2] lg:min-h-full flex items-end card overflow-hidden">
+          <picture className="relative inline-block z-30">
+            <source
+              srcset="/person_2.png"
+              aria-hidden="true"
+              alt=""
+              media="(max-width:850px)"
+            />
+            <img
+              src="/person_2@2x.png"
+              alt=""
+             className="brightness-90 md:max-w-[300px] relative z-20 transition-all duration-300 ease-in-out"
+            />
+          </picture>
+          <div className="absolute bottom-5 left-5 text-[#FFFEFE] z-50">
+            <h3 className="font-bold text-xl">Sophia Moretti</h3>
+            <h4 className="text-md mt-2">Business Development Manager</h4>
+          </div>
         </div>
-        <div>
-          <img src="" alt="" />
-          <h3>Ethan Rossi</h3>
-          <h4>Business Development Manager</h4>
+        <div className="w-full relative min-h-[300px]  bg-[#9DC4F8] lg:min-h-full flex items-end card overflow-hidden">
+          <picture className="relative inline-block z-30">
+            <source
+              srcset="/person_3.png"
+              aria-hidden="true"
+              alt=""
+              media="(max-width:850px)"
+            />
+            <img
+              src="/person_3@2x.png"
+              alt=""
+              className="brightness-90 md:max-w-[300px] relative z-20 transition-all duration-300 ease-in-out"
+            />
+          </picture>
+          <div className="absolute bottom-5 left-5 text-[#FFFEFE]  z-50">
+            <h3 className="font-bold text-xl">Ethan Rossi</h3>
+            <h4 className="text-md mt-2">Business Development Manager</h4>
+          </div>
         </div>
-        <div>
-          <img src="" alt="" />
-          <h3>Isabella Ricci</h3>
-          <h4>UX Designer</h4>
+        <div className="w-full relative min-h-[288px]   bg-[#F8D8B0] lg:min-h-full flex items-end card overflow-hidden">
+          <picture className="relative inline-block z-30">
+            <source
+              srcset="/person_4.png"
+              aria-hidden="true"
+              alt=""
+              media="(max-width:850px)"
+            />
+            <img
+              src="/person_4@2x.png"
+              alt=""
+              className="brightness-90 md:max-w-[300px] relative z-20 transition-all duration-300 ease-in-out"
+            />
+          </picture>
+          <div className="absolute bottom-5 left-5 text-[#FFFEFE] z-50">
+            <h3 className="font-bold text-xl">Isabella Ricci</h3>
+            <h4 className="text-md mt-2">UX Designer</h4>
+          </div>
         </div>
-        <div>
-          <img src="" alt="" />
-          <h3>Noah Conti</h3>
-          <h4>Content Creator</h4>
+        <div className="w-full relative min-h-[288px]   bg-[#86E7D4]  lg:min-h-full flex items-end card overflow-hidden">
+          <picture className="relative inline-block z-30">
+            <source
+              srcset="/person_5.png"
+              aria-hidden="true"
+              alt=""
+              media="(max-width:850px)"
+            />
+            <img
+              src="/person_5@2x.png"
+              alt=""
+              className="brightness-90 md:max-w-[300px]  relative z-50"
+            />
+          </picture>
+          <div className="absolute bottom-5 left-5 text-[#FFFEFE] z-50">
+            <h3 className="font-bold text-xl">Noah Conti</h3>
+            <h4 className="text-md mt-2">Content Creator</h4>
+          </div>
         </div>
-        <div>
-          <a href="#none">See all members <img src={arrowUp} alt="" /></a>
+        <div className="bg-[#F3F4F6] w-full flex items-end min-h-[304px] p-8 lg:col-start-1 lg:col-end-2 lg:row-start-2 lg:row-end-3">
+          <a href="#none" className="flex items-center gap-2 transition-all border-2 border-transparent duration-300 hover:text-red-400 hover:border-b-2 hover:border-b-red-500 ">
+            See all members <img src={arrowUp} alt="" />
+          </a>
         </div>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
